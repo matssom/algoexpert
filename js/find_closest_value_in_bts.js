0d4dfcb468d196d8e181db05972c7f882b7ba894
Mats Sommervold <mats@sommervold.net>
2021-03-28 20:06:22 +0200

    Completing 'find closest value in bts

---
+++ js/find_closest_value_in_bts.js
@@ -0,0 +1,13 @@
+// given a binary search tree and a target value. Find the closest match
+
+function findClosestValueInBst(tree, target) {
+    let closest = tree
+    let current = tree
+    while (true) {
+        if (Math.abs(current.value - target) < Math.abs(closest.value - target)) closest = current
+        if (target > current.value) current = current.right
+        else current = current.left
+        if (!current) break
+    }
+    return closest.value
+}
