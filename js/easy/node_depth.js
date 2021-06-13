4ba45d3ccdd6fd6481057699d5d6c7abe83065a8
Mats Sommervold <mats@sommervold.net>
2021-06-13 20:56:15 +0200

    :white_check_mark: Implemented node depth algorithm

---
+++ js/easy/node_depth.js
@@ -0,0 +1,14 @@
+// Sum all the node depth's of a binary tree
+
+const nodeDepthRecursive = (node, depth) => {
+	if (!node) return 0
+	let sum = depth
+	const nextDepth = depth + 1
+	sum += nodeDepthRecursive(node.left, nextDepth)
+	sum += nodeDepthRecursive(node.right, nextDepth)
+	return sum
+}
+
+const nodeDepths = (root) => {
+  return nodeDepthRecursive(root, 0)
+}
