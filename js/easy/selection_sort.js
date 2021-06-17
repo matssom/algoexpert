53f634295fc74dd57caf16ebc30b98a4884c6664
Mats Sommervold <mats@sommervold.net>
2021-06-17 17:57:55 +0200

    :sparkles: Implement selection sort

---
+++ js/easy/selection_sort.js
@@ -0,0 +1,16 @@
+// Selection sort algorithm
+
+function selectionSort(array) {
+	for (let i = 0; i < array.length; i++) {
+		let min = i
+		for (let j = i; j < array.length; j++) {
+			if (array[j] < array[min]) min = j
+		}
+		if (array[min] < array[i]) {
+			const temp = array[i]
+			array[i] = array[min]
+			array[min] = temp
+		}
+	}
+	return array
+}
