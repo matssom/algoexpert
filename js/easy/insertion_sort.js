11e04501bee861223f9725736d175da1c37805cd
Mats Sommervold <mats@sommervold.net>
2021-06-17 17:57:29 +0200

    :recycl: Update from selection sort to insertion sort

--- js/easy/insertion_sort.js
+++ js/easy/insertion_sort.js
@@ -1,16 +1,18 @@
 // Insertion sort algorithm
 
 function insertionSort(array) {
-	for (let i = 0; i < array.length; i++) {
-		let min = i
-		for (let j = i; j < array.length; j++) {
-			if (array[j] < array[min]) min = j
-		}
-		if (array[min] < array[i]) {
-			const temp = array[i]
-			array[i] = array[min]
-			array[min] = temp
+	for (let i = 1; i < array.length; i++) {
+		let j = i
+		while (j > 0 && array[j] < array[j - 1]) {
+			swap(j, j - 1, array)
+			j -= 1
 		}
 	}
 	return array
+}
+
+function swap(i, j, array) {
+	const temp = array[j]
+	array[j] = array[i]
+	array[i] = temp
 }
