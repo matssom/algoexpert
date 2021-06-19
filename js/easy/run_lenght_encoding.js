bf2b57717f9f3eb565f63f90dfd1320a2831966f
Mats Sommervold <mats@sommervold.net>
2021-06-19 14:05:07 +0200

    :recycle: Refactor run length encoding to be cleaner

--- js/easy/run_lenght_encoding.js
+++ js/easy/run_lenght_encoding.js
@@ -2,23 +2,41 @@
 
 function runLengthEncoding(string) {
 	const res = []
-	let current,
+	let current = string.charAt(0),
 		count = 0
 	for (const char of string) {
-		if (char === current) count++
-		else {
-			res.push(encode(count, current))
+		if (char !== current || count === 9) {
+			res.push(`${count}${current}`)
+			count = 0
 			current = char
-			count = 1
 		}
+		count++
 	}
-	res.push(encode(count, current))
+	res.push(`${count}${current}`)
 	return res.join('')
 }
 
-function encode(amount, char) {
-	if (!char) return ''
-	let x = Math.floor(amount / 9)
-	let y = amount % 9
-	return `${`9${char}`.repeat(x)}${y > 0 ? `${y}${char}` : ''}`
-}
+// V1 (slightly more work)
+
+// function runLengthEncoding(string) {
+// 	const res = []
+// 	let current,
+// 		count = 0
+// 	for (const char of string) {
+// 		if (char === current) count++
+// 		else {
+// 			res.push(encode(count, current))
+// 			current = char
+// 			count = 1
+// 		}
+// 	}
+// 	res.push(encode(count, current))
+// 	return res.join('')
+// }
+
+// function encode(amount, char) {
+// 	if (!char) return ''
+// 	let x = Math.floor(amount / 9)
+// 	let y = amount % 9
+// 	return `${`9${char}`.repeat(x)}${y > 0 ? `${y}${char}` : ''}`
+// }
